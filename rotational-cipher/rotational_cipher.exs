191e5d8574fb0ae332963c598f3478cabccd1112
jbalcita <jbalcita@cj.com>
2018-02-06 00:18:28 -0800

    minor aesthetic adjustments

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -9,17 +9,18 @@ defmodule RotationalCipher do
 
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
-    alphabet =  ?a..?z |> Enum.to_list |> List.to_string |> String.split("", trim: true)
-    cipher_map = create_cipher_map(alphabet, rem(shift, 26))
+    cipher_map = ?a..?z
+    |> Enum.to_list |> List.to_string |> String.split("", trim: true)
+    |> create_cipher_map(rem shift, 26)
 
-    Regex.replace(~r/[a-zA-Z]/, text, fn x -> cipher_map[x] end)
+    Regex.replace ~r/[a-zA-Z]/, text, fn x -> cipher_map[x] end
   end
 
   defp create_cipher_map(alphabet, shift) do
     {fst, snd} = alphabet |> Enum.split(shift)
     tuple = alphabet |> Enum.zip(Enum.concat(snd, fst))
 
-    Enum.concat(tuple, Enum.map(tuple, fn ({a, b}) -> {String.upcase(a), String.upcase(b)} end))
+    tuple |> Enum.concat(Enum.map(tuple, fn ({a, b}) -> {String.upcase(a), String.upcase(b)} end))
     |> Enum.into(%{})
   end
 end

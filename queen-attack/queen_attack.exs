b7ff9e7d2dbe81f0cdbb4bcc30995ed49b1401ef
jbalcita <jbalcita@cj.com>
2018-02-07 18:17:57 -0800

    more pattern matching

--- queen-attack/queen_attack.exs
+++ queen-attack/queen_attack.exs
@@ -18,10 +18,10 @@ defmodule Queens do
   white and black queen locations shown
   """
   @spec to_string(Queens.t()) :: String.t()
-  def to_string(%Queens{black: black, white: white} = queens) do
+  def to_string(%Queens{black: {row, col}, white: {row_, col_}} = queens) do
     List.duplicate("_", 8) |> List.duplicate(8)
-    |> List.update_at(elem(black, 0), fn b -> List.update_at(b, elem(black, 1), fn _ -> "B" end) end)
-    |> List.update_at(elem(white, 0), fn w -> List.update_at(w, elem(white, 1), fn _ -> "W" end) end)
+    |> List.update_at(row,  fn b -> List.update_at(b, col,  fn _ -> "B" end) end)
+    |> List.update_at(row_, fn w -> List.update_at(w, col_, fn _ -> "W" end) end)
     |> Enum.map(&Enum.join(&1, " "))
     |> Enum.join("\n")
   end

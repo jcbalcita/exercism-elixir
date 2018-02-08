febcb1071b5e546955d1046b50026cc18be5349f
jbalcita <jbalcita@cj.com>
2018-02-08 10:30:44 -0800

    no need to create a map, just index in

--- twelve-days/twelve_days.exs
+++ twelve-days/twelve_days.exs
@@ -3,21 +3,15 @@ defmodule TwelveDays do
   Given a `number`, return the song's verse for that specific day, including
   all gifts for previous days in the same line.
   """
-  @day Enum.to_list(1..12)
-       |> Enum.zip(["first", "second", "third", "fourth", "fifth", "sixth","seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"])
-       |> Enum.into(%{})
+  @day ~w(_ first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth)
 
-  @gift Enum.to_list(1..12)
-        |> Enum.zip(["and a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens", "four Calling Birds",
-                    "five Gold Rings", "six Geese-a-Laying", "seven Swans-a-Swimming", "eight Maids-a-Milking",
-                    "nine Ladies Dancing", "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"])
-        |> Enum.into(%{})
-
-  @first_part "On the _count_ day of Christmas my true love gave to me, "
+  @gift ["_", "and a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens", "four Calling Birds",
+        "five Gold Rings", "six Geese-a-Laying", "seven Swans-a-Swimming", "eight Maids-a-Milking",
+        "nine Ladies Dancing", "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"]
 
   @spec verse(number :: integer) :: String.t()
-  def verse(1), do: format_first_part()  <> "a Partridge in a Pear Tree."
-  def verse(n), do: format_first_part(n) <> descending_gift_list(n) <> "."
+  def verse(1), do: first_part(1)  <> "a Partridge in a Pear Tree."
+  def verse(n), do: first_part(n) <> descending_gift_list(n) <> "."
 
   @doc """
   Given a `starting_verse` and an `ending_verse`, return the verses for each
@@ -32,7 +26,7 @@ defmodule TwelveDays do
   @spec sing():: String.t()
   def sing, do: verses(1, 12)
 
-  defp format_first_part(day \\ 1), do: String.replace(@first_part, ~r/_count_/, @day[day])
-  defp descending_gift_list(number), do: number..1 |> Enum.map(fn n -> @gift[n] end) |> Enum.join(", ")
+  defp first_part(number), do: "On the #{Enum.at(@day, number)} day of Christmas my true love gave to me, "
+  defp descending_gift_list(number), do: number..1 |> Enum.map(fn n -> Enum.at(@gift, n) end) |> Enum.join(", ")
 end
 
